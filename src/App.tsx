5907fbbe8182cf26d5c72ef81209d9c7f21b7905
Maha_Balan2301 <mahalakshmi.b2022a@vitstudent.ac.in>
2026-04-24 14:41:34 +0530

    feat: make workflow layout responsive for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import WorkflowCanvas from './components/WorkflowCanvas';
 
 export default function App() {
   return (
-    <main style={{ width: '100vw', height: '100vh' }}>
+    <main style={{ width: '100%', height: '100dvh' }}>
       <WorkflowCanvas />
     </main>
   );
